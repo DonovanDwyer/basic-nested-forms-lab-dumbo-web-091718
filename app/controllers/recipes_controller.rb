13c755c33f65e94e9e3c755b0ff34b678e2ad13f
DonovanDwyer <dwyer.donovan@gmail.com>
2018-10-16 16:40:02 -0400

    built nested form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,27 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'First Ingredient')
+    @recipe.ingredients.build(name: 'Second Ingredient')
   end
 
   def create
+    @recipe = Recipe.create(title: recipe_params[:title])
+    @recipe.ingredients.create(recipe_params[:ingredients_attributes]["0"])
+    @recipe.ingredients.create(recipe_params[:ingredients_attributes]["1"])
+    # byebug
+    redirect_to @recipe
   end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(
+        :title,
+        ingredients_attributes: [
+          :name,
+          :quantity
+        ]
+      )
+    end
 end
